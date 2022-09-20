60c8dfa64c30253d08b0abb3f93e575fe22ab488
RayanCR <113985143+RayanCR@users.noreply.github.com>
2022-09-20 15:54:42 +0200

    Update Launcher.java

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,18 +2,51 @@ import java.util.Scanner;
 
 public class Launcher
 {
+	public static int fibo(int n)
+	{
+		int i;
+
+		int f0 = 0;
+		int f1 = 1;
+		int fn = 0;
+
+		for (i = 2; i<n+1; i++)
+		{
+			fn = f0 + f1;
+			f0 = f1;
+			f1 = fn;
+		}
+
+		return fn;
+	}
+
 	public static void main(String[] args) 
 	{
+		
 		System.out.println("Bienvenue");	
 		
 		var scanner = new Scanner(System.in);
 
-		String input = scanner.nextLine();
+		String input = scanner.nextLine();;
+
+		int val;
 
 		while (!"quit".equals(input))
 		{
-			System.out.println("unknown command");
+			if ("fibo".equals(input))
+			{
+				System.out.println("Entree un nombre : ");
+				val = scanner.nextInt();
+				input = scanner.nextLine();
+				System.out.println("Fibo de " + val + " est egale a : " + fibo(val));
+			}
+			else
+			{
+				System.out.println("unknown command");
+			}
+
 			input = scanner.nextLine();
 		}
+		
 	}
 }

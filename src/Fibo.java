72c7d440baad19b9cfb905f2db692bc1dd63ca36
RayanCR <113985143+RayanCR@users.noreply.github.com>
2022-09-27 15:20:07 +0200

    Update Fibo.java

--- src/Fibo.java
+++ src/Fibo.java
@@ -1,34 +1,10 @@
 import java.util.Scanner;
 
-public static int fibo(int n)
-{
-    int i;
-
-    int f0 = 0;
-    int f1 = 1;
-    int fn = 0;
-
-    for (i = 2; i<n+1; i++)
-    {
-        fn = f0 + f1;
-        f0 = f1;
-        f1 = fn;
-    }
-
-    return fn;
-}
-
 abstract class Fibo implements Command{
     public String name () {return "fibo";}
     public Boolean run(Scanner scanner){
         
-        System.out.print("Entree un nombre : ");
-		val = scanner.nextInt();
-        input = scanner.nextLine();
-        System.out.println("Fibo de " + val + " est egale a : " + fibo(val));
-        
-        /*
-        System.out.println("Entrez l'index du nombre que vous chercher dans la séquence de fibo : ");
+        System.out.println("Entrez un nombre : ");
         int n = Integer.parseInt(scanner.nextLine());
         if(n <= 1) {
             System.out.println("Le résultat est "+n);
@@ -44,6 +20,6 @@ abstract class Fibo implements Command{
         }
         System.out.println("Le résultat est "+fib);
         return false;
-        */
+        
     }
 }

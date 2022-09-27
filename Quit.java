fe1109c278fec1162c50090963363acd0475df79
RayanCR <113985143+RayanCR@users.noreply.github.com>
2022-09-27 15:05:00 +0200

    Delete Quit.java

--- Quit.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-abstract class Quit implements Command {
-    public String name () {return "quit";}
-    public Boolean run(Scanner scanner){
-        return true;
-    }
-}

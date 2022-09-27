aa11989cc1205051c1763fbc6b65b2c742959a4d
RayanCR <113985143+RayanCR@users.noreply.github.com>
2022-09-27 15:12:01 +0200

    Update Freq.java

--- src/Freq.java
+++ src/Freq.java
@@ -3,44 +3,19 @@ import java.util.*;
 abstract class Freq implements Command {
     public String name () {return "freq";}
     public Boolean run(Scanner scanner){
-        System.out.println("Entrez le path du fichier que vous voulez traiter (connaitre les 3 mots les plus courants) : ");
+        System.out.print("Entrez le path d'un fichier : ");
         Scanner sc = new Scanner(System.in);
-        String inpf = (sc.nextLine());
-        try {
-            String fc = java.nio.file.Files.readString(Paths.get(inpf));
-            fc = fc.replaceAll("\\p{Punct}", " ");
-            fc = fc.toLowerCase();
-            String[] fct = fc.split(" ");
-            ArrayList<String> fct2 = new ArrayList<>();
-            for (String word : fct){
-                if(!word.isBlank()){
-                    fct2.add(word);
-                }
-            }
-            Map<String,Integer> map_res = new HashMap<>();
-            int i;
-            for (String word : fct2){
-                i=0;
-                for (String wordc : fct2){
-                    if(word.equals(wordc)){
-                        i++;
-                    }
-                }
-                map_res.put(word,i);
-            }
-            for(int c=0; c<3; c++) {
-                int maxValueInMap = (Collections.max(map_res.values()));
-                for (Map.Entry<String, Integer> map : map_res.entrySet()) {
-                    if (map.getValue() == maxValueInMap) {
-                        System.out.print(map.getKey()+" ");
-                        map_res.remove(map.getKey());
-                        break;
-                    }
-                }
-            }
-            System.out.println();
-        }catch(Exception e){
-            System.out.println("Unreadable file: "+e);
+        String path = (sc.nextLine());
+       try 
+       {
+          Path file = Paths.get(path);
+          String content = Files.readString(file);
+          String words[] = content.replaceAll("\\W", " ").toLowerCase().split(" ");
+          Map<String, Long> countsByWords = Arrays.stream(words).map(w -> w.trim()).filter(w -> w.length() > 0).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+          System.out.println(countsByWords.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed()).map(e -> e.getKey()).limit(3).collect(Collectors.joining(" ")));
+        } 
+        catch(Exception exception) {
+          System.out.println("Unreadable file: " + exception);
         }
         return false;
     }
